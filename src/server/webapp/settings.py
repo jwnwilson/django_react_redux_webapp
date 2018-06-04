c9e40a7232b95d1202f9d126d4b10a215fdcf080
Noel Wilson <noel.wilson@rehabstudio.com>
2018-06-04 17:01:16 +0100

    Adding missing comma and whitenoise to wsgi

--- src/server/webapp/settings.py
+++ src/server/webapp/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'polymorphic',
     'webpack_loader',
