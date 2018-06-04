6d2fcd826bc20b283df388382f42ea1c41625d8d
Noel Wilson <noel.wilson@rehabstudio.com>
2018-06-04 16:56:56 +0100

    Adding whitenoise dev app

--- src/server/webapp/settings.py
+++ src/server/webapp/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic'
     'django.contrib.staticfiles',
     'polymorphic',
     'webpack_loader',
