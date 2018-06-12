72f5fc6d214a9035856a6b522075ec715faa8de8
Noel Wilson <noel.wilson@rehabstudio.com>
2018-06-12 10:35:14 +0100

    Adding margings to wrapimage

--- src/client/src/components/wrapImage/index.js
+++ src/client/src/components/wrapImage/index.js
@@ -11,10 +11,10 @@ class WrapImage extends React.Component {
     return (
       <section className="bg-primary text-white mb-0 mt-5" id="about">
         <div className="container">
-          <div className="pull-left col-xs-12 col-sm-5">
+          <div className="pull-left col-12 col-md-5">
             <img className="img-responsive rounded" src={module.image.file} alt={module.title} width="100%"/>
           </div>
-          <div className="pull-left col-xs-12 col-sm-7">
+          <div className="pull-left col-12 col-md-7 mt-3">
             <h2>{module.title}</h2>
           </div>
           {paras}
