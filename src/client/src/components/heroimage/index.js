9032308d80c35d9b60c1f3d92ad2f494c635b0d2
Noel Wilson <noel.wilson@rehabstudio.com>
2018-06-12 10:32:41 +0100

    Formatted image wrap

--- src/client/src/components/heroimage/index.js
+++ src/client/src/components/heroimage/index.js
@@ -8,7 +8,7 @@ class HeroImage extends React.Component {
     return (
      <header id="intro" className="masthead bg-primary text-white text-center">
        <div className="container">
-         <img className="img-fluid rounded-circle w-25 mb-5 d-block mx-auto" src={data.image.file} alt="" />
+         <img className="img-fluid rounded-circle col-10 col-sm-8 col-md-6 col-lg-4 mb-5 d-block mx-auto" src={data.image.file} alt="" />
          <h1 className="text-uppercase mb-0">{data.title}</h1>
          <hr className="star-light" />
          <h2 className="font-weight-light mb-0">{data.text}</h2>
--- src/client/src/components/wrapImage/index.js
+++ src/client/src/components/wrapImage/index.js
@@ -4,20 +4,20 @@ import './../../style/WrapImage.css'
 class WrapImage extends React.Component {
   render () {
     let module = this.props.data.module;
+    let paras = module.text.split('\n').map((para, index) => {
+      return (<p key={index}>{para}</p>);
+    });
+
     return (
-      <section className="bg-primary text-white mb-0" id="about">
+      <section className="bg-primary text-white mb-0 mt-5" id="about">
         <div className="container">
-          <div className="col-xs-12 col-sm-5">
-            <img className="img-responsive" src={module.image.file} alt={module.title} width="100%"/>
+          <div className="pull-left col-xs-12 col-sm-5">
+            <img className="img-responsive rounded" src={module.image.file} alt={module.title} width="100%"/>
           </div>
-          <div className="col-xs-12 col-sm-7">
-            <div className="row">
-              <div className="col-sm-6">
-                <h2>{module.title}</h2>
-              </div>
+          <div className="pull-left col-xs-12 col-sm-7">
+            <h2>{module.title}</h2>
           </div>
-          </div>
-          {module.text}
+          {paras}
           <div className="col-xs-12">
             <hr />
           </div>
