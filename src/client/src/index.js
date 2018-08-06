33c7c4a200ae095311030418813470693029a60b
Noel Wilson <noel.wilson@rehabstudio.com>
2018-08-06 09:28:11 +0100

    Fixing arguments in wrong order

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -5,13 +5,13 @@ import { Provider } from 'react-redux';
 
 import store from './store';
 import registerServiceWorker from './registerServiceWorker';
-import getRoutes from './App/App.routes';
+import appRoutes from './App/App.routes';
 import './style/index.css';
 
 const root = document.getElementById('root');
 const apiData = JSON.parse(root.getAttribute('data-api'));
 const pagesData = JSON.parse(root.getAttribute('data-pages'));
-const routes = getRoutes(pagesData, apiData);
+const routes = appRoutes.getRoutes(apiData, pagesData);
 
 render(
   <Provider store={store}>
