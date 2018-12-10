19c04f321a27451a7c82aea5e7447e18333e7363
Noel Wilson <jwnwilson@hotmail.co.uk>
2018-12-10 22:21:03 +0000

    Rendering blog data fromREST data

--- src/client/src/components/Blog/index.js
+++ src/client/src/components/Blog/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import ReactMarkdown from 'react-markdown';
+import renderHTML from 'react-render-html';
 import PropTypes from 'prop-types';
 
 import '../../style/core.css';
@@ -10,36 +10,32 @@ const Blog = (props) => {
 
   // Build blog post
   const blogData = JSON.parse(module.body);
-  const blogPost = blogData.map((paragraph, index) => {
-    return null;
-    // const paras = (
-    //   <ReactMarkdown source={paragraph.text} />
-    // );
-    // return (
-    //   <div className="clear-fix">
-    //     <div key={index} className="pull-left col-12 col-md-5 mb-3">
-    //       <img
-    //             className="img-responsive rounded"
-    //             src={img}
-    //             alt={module.title}
-    //             width="100%"
-    //             style={{ width: '100%' }}
-    //           />
-    //     </div>
-    //     {paras}
-    //   </div>
-    // );
+  const blogPost = blogData.map((block, index) => {
+    let blogElement = null;
+    switch (block.type) {
+      case 'heading':
+        blogElement = (
+          <React.Fragment key={index}>
+            <h1>{block.value}</h1>
+            <hr />
+          </React.Fragment>
+        );
+        break;
+      case 'paragraph':
+        blogElement = (
+          <React.Fragment key={index}>
+            {renderHTML(block.value)}
+          </React.Fragment>
+        );
+        break;
+      default:
+    }
+    return blogElement;
   });
 
   return (
     <section className="wrap-image bg-primary text-white mb-0 mt-5" id="about">
       <div className="container">
-        <div className="pull-left col-12 col-md-7 mt-3">
-          <h2>
-            {module.title}
-          </h2>
-          <hr />
-        </div>
         {blogPost}
         <div className="clear-fix col-xs-12">
           <hr />
